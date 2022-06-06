876cf82827c72b201d759223fccd183db38e1efe
AthulJk-h <76027722+AthulJk-h@users.noreply.github.com>
2022-06-06 14:08:13 +0530

    Update and rename OverloadShapes.java to shapes.java

---
+++ shapes.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class shapes {
+   
+    void area(int r1){
+        double Area_val = 3.14*r1*r1;
+        System.out.println("\nArea of Circle is Radius "+r1+" = "+Area_val);
+    }
+    void area(int a1,int b1){
+        int Area_val = a1*b1;
+        System.out.println("\nArea of Rectangle is with dimensions "+a1+" X "+b1+" = "+Area_val);
+    }
+    void area(int a1,int b1,int c1){
+        int Area_val = a1*b1*c1;
+        System.out.println("\nArea of Cuboid is with dimensions "+a1+" X "+b1+" X "+c1+" = "+Area_val);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter the Length");
+        int l = sc.nextInt();
+        System.out.println("Enter the Breath");
+        int b = sc.nextInt();
+        System.out.println("Enter the Height");
+        int h = sc.nextInt();
+        System.out.println("Enter the Radius");
+        int r = sc.nextInt();
+        shapes obj1 = new shapes();
+        obj1.area(r);
+        obj1.area(l,b);
+        obj1.area(l,b,h);
+    }
+
+}
